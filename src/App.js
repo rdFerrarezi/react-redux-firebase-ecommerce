5d8419052edccc9b353e0aa33d3c4d8a6d86fe07
rdFerrarezi <canalferrarezzi@gmail.com>
2022-01-11 17:55:39 -0300

    Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from './components/Header';
+import './default.scss';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <h2>Hellow world</h2>
     </div>
   );
 }
